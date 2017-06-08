5b32b1272cbeeed93bd76f76bf2895740a95695c
Ao Song <ao.song@outlook.com>
2017-06-08 14:43:57 +0200

    update

--- InetAddress.cpp
+++ InetAddress.cpp
@@ -6,98 +6,98 @@ using namespace Tube;
 
 bool
 InetAddress::init(
-	const char* ipStr)
+    const char* ipStr)
 {
-	skip_spaces(ipStr);
+    skip_spaces(ipStr);
 
-	const char* startOfAddress = ipStr;
+    const char* startOfAddress = ipStr;
 
-	enum {None, IPv4, IPv6} ipVersion = None;
+    enum {None, IPv4, IPv6} ipVersion = None;
 
-	while(true)
-	{
-		switch(ipVersion)
-		{
-			case None:
-			{				
-				if(*ipStr == '.')
-				{
-					ipVersion = IPv4;
-				}
-				else if(isxdigit(*ipStr) || *ipStr == ':')
-				{
-					ipVersion = IPv6
-				}
-				else if(!isdigit(ipStr))
-				{
-					// what is this then?
-					return false;
-				}
-				break;
-			}
-			case IPv4:
-			{
-				if(!(isdigit(*ipStr) || (*ipStr == '.')))
-				{
-					size_t addressLength = ipStr - startOfAddress;
-					if(addressLength > 15)
-					{
-						// not an IP address
-						return false;
-					}
+    while(true)
+    {
+        switch(ipVersion)
+        {
+            case None:
+            {               
+                if(*ipStr == '.')
+                {
+                    ipVersion = IPv4;
+                }
+                else if(isxdigit(*ipStr) || *ipStr == ':')
+                {
+                    ipVersion = IPv6
+                }
+                else if(!isdigit(ipStr))
+                {
+                    // what is this then?
+                    return false;
+                }
+                break;
+            }
+            case IPv4:
+            {
+                if(!(isdigit(*ipStr) || (*ipStr == '.')))
+                {
+                    size_t addressLength = ipStr - startOfAddress;
+                    if(addressLength > 15)
+                    {
+                        // not an IP address
+                        return false;
+                    }
 
-					unsigned char buf[16];
-					memcpy(buf, startOfAddress, addressLength);
-					buf[addressLength] = 0;
+                    unsigned char buf[16];
+                    memcpy(buf, startOfAddress, addressLength);
+                    buf[addressLength] = 0;
 
-					skip_spaces(ipStr);
-					if(*ipStr != 0)
-					{
-						return false;
-					}
+                    skip_spaces(ipStr);
+                    if(*ipStr != 0)
+                    {
+                        return false;
+                    }
 
-					memset(&addressM.addrIn4, 0, sizeof(struct sockaddr_in));
-					addressM.addrIn4.sin_family = AF_INET;
-					return inet_pton(
-						AF_INET, buf, &addressM.addrIn4.sin_addr) == 1;
-				}
-				break;
-			}
-			case IPv6:
-			{
-				if(!(isxdigit(*ipStr) || (*ipStr == ':')))
-				{
-					size_t addressLength = ipStr - startOfAddress;
-					if(addressLength > 39)
-					{
-						return false;
-					}
+                    memset(&addressM.addrIn4, 0, sizeof(struct sockaddr_in));
+                    addressM.addrIn4.sin_family = AF_INET;
+                    return inet_pton(
+                        AF_INET, buf, &addressM.addrIn4.sin_addr) == 1;
+                }
+                break;
+            }
+            case IPv6:
+            {
+                if(!(isxdigit(*ipStr) || (*ipStr == ':')))
+                {
+                    size_t addressLength = ipStr - startOfAddress;
+                    if(addressLength > 39)
+                    {
+                        return false;
+                    }
 
-					unsigned char buf[40];
-					memcpy(buf, startOfAddress, addressLength);
-					buf[addressLength] = 0;
+                    unsigned char buf[40];
+                    memcpy(buf, startOfAddress, addressLength);
+                    buf[addressLength] = 0;
 
-					skip_spaces(ipStr);
-					if(*ipStr != 0)
-					{
-						return false;
-					}
+                    skip_spaces(ipStr);
+                    if(*ipStr != 0)
+                    {
+                        return false;
+                    }
 
-					memset(&addressM.addrIn6, 0, sizeof(struct sockaddr_in6));
-					addressM.addrIn6.sin6_family = AF_INET6;
-					return inet_pton(
-						AF_INET6, buf, &addressM.addrIn6.sin6_addr) == 1;
-				}
-				break;
-			}
-			default:
-			{
-				return false;
-			}
-		}
+                    memset(&addressM.addrIn6, 0, sizeof(struct sockaddr_in6));
+                    addressM.addrIn6.sin6_family = AF_INET6;
+                    return inet_pton(
+                        AF_INET6, buf, &addressM.addrIn6.sin6_addr) == 1;
+                }
+                break;
+            }
+            default:
+            {
+                return false;
+            }
+        }
 
-		ipStr++;
-	}
+        ipStr++;
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -105,36 +105,36 @@ InetAddress::init(
 bool
 InetAddress::init(
     const struct sockaddr_storage* address,
-	socklen_t                      addressLength)
+    socklen_t                      addressLength)
 {
-	if(reinterpret_cast<const struct sockaddr*>(address)->sa_family == AF_INET)
-	{
-		// IPv4
-		const struct sockaddr_in* addrV4 = 
-		    reinterpret_cast<const struct sockaddr_in*>(address);
-		if(addressLength <= sizeof(addressM))
-		{
-			memcpy(&addressM, addrV4, addressLength);
-		}
-		else
-		{
-			return false;
-		}
-	}
-	else
-	{
-		// IPv6
-		const struct sockaddr_in6 addrV6 = 
-		    reinterpret_cast<const struct sockaddr_in6*>(address);
-		if(addressLength <= sizeof(addressM))
-		{
-		 	memcpy(&addressM, addrV6, addressLength);
-		}
-		else
-		{
-		   	return false;
-		}
-	}
+    if(reinterpret_cast<const struct sockaddr*>(address)->sa_family == AF_INET)
+    {
+        // IPv4
+        const struct sockaddr_in* addrV4 = 
+            reinterpret_cast<const struct sockaddr_in*>(address);
+        if(addressLength <= sizeof(addressM))
+        {
+            memcpy(&addressM, addrV4, addressLength);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        // IPv6
+        const struct sockaddr_in6 addrV6 = 
+            reinterpret_cast<const struct sockaddr_in6*>(address);
+        if(addressLength <= sizeof(addressM))
+        {
+            memcpy(&addressM, addrV6, addressLength);
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
--- InetAddress.h
+++ InetAddress.h
@@ -6,193 +6,193 @@
 
 namespace Tube
 {
-	class InetAddress
-	{
-	public:
-		InetAddress();
+    class InetAddress
+    {
+    public:
+        InetAddress();
 
-		InetAddress(
-			const InetAddress& other);
+        InetAddress(
+            const InetAddress& other);
 
-		InetAddress(
-			const struct sockaddr* address,
-			socklen_t              addressLength);
+        InetAddress(
+            const struct sockaddr* address,
+            socklen_t              addressLength);
 
-		InetAddress&
-		operator=(
-			const InetAddress& other);
+        InetAddress&
+        operator=(
+            const InetAddress& other);
 
-		~InetAddress();
+        ~InetAddress();
 
-		bool
-		operator==(
-			const InetAddress& other) const;
+        bool
+        operator==(
+            const InetAddress& other) const;
 
-		bool
-		init(
-			const std::string& ipStr);
+        bool
+        init(
+            const std::string& ipStr);
 
-		bool
-		init(
-			const char* ipStr);
+        bool
+        init(
+            const char* ipStr);
 
-		bool
-		init(
-			const struct sockaddr_storage* address,
-			socklen_t                      addressLength,
-			unsigned short&                port);
+        bool
+        init(
+            const struct sockaddr_storage* address,
+            socklen_t                      addressLength,
+            unsigned short&                port);
 
-		void
-		set_port(
-			unsigned short port);
+        void
+        set_port(
+            unsigned short port);
 
-		const struct sockaddr*
-		get_address() const;
+        const struct sockaddr*
+        get_address() const;
 
-		socklen_t
-		get_address_length() const;
+        socklen_t
+        get_address_length() const;
 
-		void
-		get_socket_address(
-			int&              inetFamily,
-			sockaddr_storage& address,
-			unsigned short    port,
-			socklen_t         addressLength) const;
+        void
+        get_socket_address(
+            int&              inetFamily,
+            sockaddr_storage& address,
+            unsigned short    port,
+            socklen_t         addressLength) const;
 
-	private:
-		void
-		skip_spaces(
-			const char*& str);
+    private:
+        void
+        skip_spaces(
+            const char*& str);
 
-	private:
-		union Address
-		{
-			struct sockaddr_in  addrIn4;
-			struct sockaddr_in6 addrIn6;
-			struct sockaddr     addrRaw;
-		}addressM;
-	};
-
-	inline
-	InetAddress::InetAddress()
-	{
-		memset(&addressM, 0, sizeof(addressM));
-	}
+    private:
+        union Address
+        {
+            struct sockaddr_in  addrIn4;
+            struct sockaddr_in6 addrIn6;
+            struct sockaddr     addrRaw;
+        }addressM;
+    };
 
     inline
-	InetAddress::InetAddress(
-		const InetAddress& other)
-	: addressM(other.addressM)
-	{
-		// Empty
-	}
+    InetAddress::InetAddress()
+    {
+        memset(&addressM, 0, sizeof(addressM));
+    }
 
     inline
-	InetAddress::InetAddress(
-		const struct sockaddr* address,
-		socklen_t              addressLength)
-	{
-		assert(addressLength <= sizeof(addressM));
-		memcpy(&addressM, address, addressLength);
-	}
-
-	inline
-	InetAddress&
-	operator=(
-		const InetAddress& other)
-	{
-		addressM = other.addressM;
-		return *this;
-	}
-
-	inline	
-	InetAddress::~InetAddress()
-	{
-		// Empty
-	}
-
-	inline
-	bool
-	InetAddress::operator==(
-		const InetAddress& other) const
-	{
-		return (addressM.addrIn4.sin_family == AF_INET) ?
-		        memcmp(&addressM.addrIn4,
-		        	   &other.addressM.addrIn4,
-		        	   sizeof(addressM.addrIn4)) == 0 :
-		        memcmp(&addressM.addrIn6,
-		        	   &other.addressM.addrIn6,
-		        	   sizeof(addressM.addrIn6)) == 0;
-	}
+    InetAddress::InetAddress(
+        const InetAddress& other)
+    : addressM(other.addressM)
+    {
+        // Empty
+    }
 
     inline
-	bool
-	InetAddress::init(
-		const std::string& ipStr)
-	{
-		return init(ipStr.c_str());
-	}
+    InetAddress::InetAddress(
+        const struct sockaddr* address,
+        socklen_t              addressLength)
+    {
+        assert(addressLength <= sizeof(addressM));
+        memcpy(&addressM, address, addressLength);
+    }
 
     inline
-	void
-	InetAddress::set_port(
-			unsigned short port)
-	{
-		(addressM.addrRaw.sa_family == AF_INET) ?
-		addressM.addrIn4.sin_port = htons(port) :
-		addressM.addrIn6.sin6_port = htons(port);		
-	}
+    InetAddress&
+    operator=(
+        const InetAddress& other)
+    {
+        addressM = other.addressM;
+        return *this;
+    }
+
+    inline  
+    InetAddress::~InetAddress()
+    {
+        // Empty
+    }
 
     inline
-	const struct sockaddr*
-	InetAddress::get_address() const
-	{
-		return &addressM.addrRaw;
-	}
+    bool
+    InetAddress::operator==(
+        const InetAddress& other) const
+    {
+        return (addressM.addrIn4.sin_family == AF_INET) ?
+                memcmp(&addressM.addrIn4,
+                       &other.addressM.addrIn4,
+                       sizeof(addressM.addrIn4)) == 0 :
+                memcmp(&addressM.addrIn6,
+                       &other.addressM.addrIn6,
+                       sizeof(addressM.addrIn6)) == 0;
+    }
 
     inline
-	socklen_t
-	InetAddress::get_address_length() const
-	{
-		return (addressM.addrRaw.sa_family == AF_INET) ?
-		       sizeof(struct sockaddr_in) :
-		       sizeof(struct sockaddr_in6);
-	}
+    bool
+    InetAddress::init(
+        const std::string& ipStr)
+    {
+        return init(ipStr.c_str());
+    }
 
     inline
-	void
-	InetAddress::get_socket_address(
-		int&              inetFamily,
-		sockaddr_storage& address,
-		unsigned short    port,
-		socklen_t         addressLength) const
-	{
-		inetFamily = addressM.addrRaw.sa_family;
-		if(inetFamily == AF_INET)
-		{
-			addressLength = sizeof(addressM.addrIn4);
-			memcpy(&address, &addressM.addrIn4, addressLength);
-			port = ntohs(((struct sockaddr_in*)&address)->sin_port);
-		}
-		else
-		{
-			addressLength = sizeof(addressM.addrIn6);
-			memcpy(&address, &addressM.addrIn6, addressLength);
-			port = ntohs(((struct sockaddr_in6*)&address)->sin6_port);
-		}
-	}
+    void
+    InetAddress::set_port(
+            unsigned short port)
+    {
+        (addressM.addrRaw.sa_family == AF_INET) ?
+        addressM.addrIn4.sin_port = htons(port) :
+        addressM.addrIn6.sin6_port = htons(port);       
+    }
+
+    inline
+    const struct sockaddr*
+    InetAddress::get_address() const
+    {
+        return &addressM.addrRaw;
+    }
+
+    inline
+    socklen_t
+    InetAddress::get_address_length() const
+    {
+        return (addressM.addrRaw.sa_family == AF_INET) ?
+               sizeof(struct sockaddr_in) :
+               sizeof(struct sockaddr_in6);
+    }
+
+    inline
+    void
+    InetAddress::get_socket_address(
+        int&              inetFamily,
+        sockaddr_storage& address,
+        unsigned short    port,
+        socklen_t         addressLength) const
+    {
+        inetFamily = addressM.addrRaw.sa_family;
+        if(inetFamily == AF_INET)
+        {
+            addressLength = sizeof(addressM.addrIn4);
+            memcpy(&address, &addressM.addrIn4, addressLength);
+            port = ntohs(((struct sockaddr_in*)&address)->sin_port);
+        }
+        else
+        {
+            addressLength = sizeof(addressM.addrIn6);
+            memcpy(&address, &addressM.addrIn6, addressLength);
+            port = ntohs(((struct sockaddr_in6*)&address)->sin6_port);
+        }
+    }
 
 
-	inline
-	void
-	InetAddress::skip_spaces(
-		const char*& str)
-	{
-		while(isspace(*str))
-		{
-			str++;
-		}
-	}
+    inline
+    void
+    InetAddress::skip_spaces(
+        const char*& str)
+    {
+        while(isspace(*str))
+        {
+            str++;
+        }
+    }
 }
 
 #endif
