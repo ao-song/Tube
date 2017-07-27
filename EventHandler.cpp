a64df735cb01d2c079ec39b932d8816d57f72074
Ao Song <ao.song@outlook.com>
2017-07-27 17:26:19 +0200

    update

--- EventHandler.cpp
+++ EventHandler.cpp
@@ -4,20 +4,6 @@ using namespace Tube;
 
 // ----------------------------------------------------------------------------
 
-inline
-EventHandler::EventHandler(
-    EventHandlerTable*  eventHandlerTable,
-    EventHandlerOwner*  eventHandlerOwner)
-:   eventHandlerTableM(eventHandlerTable),
-    eventHandlerOwnerM(eventHandlerOwner),    
-    subscribedEventsM(0),
-    socketM(-1)
-{
-    // Empty
-}
-
-// ----------------------------------------------------------------------------
-
 EventHandler::~EventHandler()
 {
    close();
@@ -25,80 +11,6 @@ EventHandler::~EventHandler()
 
 // ----------------------------------------------------------------------------
 
-inline
-void
-EventHandler::set_et()
-{
-    subscribedEventsM |= EPOLLET;    
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-void
-EventHandler::close()
-{
-    if(socketM != -1)
-    {
-        if(subscribedEventsM != 0)
-        {
-            eventHandlerTableM->delete_event(socketM);
-        }
-        ::close(socketM);
-        socketM = -1;
-    }
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-EventHandlerTable*
-EventHandler::get_table()
-{
-    return eventHandlerTableM;
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-int
-EventHandler::get_socket() const
-{
-    return socketM;
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-EventHandler::set_socket(
-    int socket)
-{
-    assert(socketM == -1);
-    socketM = socket;
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-unsigned int
-EventHandler::get_events() const
-{
-    return subscribedEventsM;
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-void
-EventHandler::reset_events(
-    unsigned int eventsToReset)
-{
-    subscribedEventsM &= (~eventsToReset);
-}
-
-// ----------------------------------------------------------------------------
-
-inline
 bool
 EventHandler::set_events(
     unsigned int eventsToSet)
@@ -128,22 +40,3 @@ EventHandler::set_events(
     return true;
 }
 
-// ----------------------------------------------------------------------------
-
-inline
-bool
-EventHandler::is_want_read() const
-{
-    return (subscribedEventsM & EPOLLIN) != 0;
-}
-
-// ----------------------------------------------------------------------------
-
-inline
-bool
-EventHandler::is_want_write() const
-{
-    return (subscribedEventsM & EPOLLOUT) != 0;
-}
-
-// ----------------------------------------------------------------------------
--- EventHandler.h
+++ EventHandler.h
@@ -91,6 +91,94 @@ namespace Tube
         unsigned int        subscribedEventsM;
         int                 socketM;
     };
+
+// ----------------------------------------------------------------------------
+
+    inline
+    EventHandler::EventHandler(
+        EventHandlerTable*  eventHandlerTable,
+        EventHandlerOwner*  eventHandlerOwner)
+    :   eventHandlerTableM(eventHandlerTable),
+        eventHandlerOwnerM(eventHandlerOwner),    
+        subscribedEventsM(0),
+        socketM(-1)
+    {
+        // Empty
+    }
+
+    inline
+    void
+    EventHandler::set_et()
+    {
+        subscribedEventsM |= EPOLLET;    
+    }
+
+    inline
+    void
+    EventHandler::close()
+    {
+        if(socketM != -1)
+        {
+            if(subscribedEventsM != 0)
+            {
+                eventHandlerTableM->delete_event(socketM);
+            }
+            ::close(socketM);
+            socketM = -1;
+        }
+    }
+
+    inline
+    EventHandlerTable*
+    EventHandler::get_table()
+    {
+        return eventHandlerTableM;
+    }
+
+    inline
+    int
+    EventHandler::get_socket() const
+    {
+        return socketM;
+    }
+
+    inline
+    EventHandler::set_socket(
+        int socket)
+    {
+        assert(socketM == -1);
+        socketM = socket;
+    }
+
+    inline
+    unsigned int
+    EventHandler::get_events() const
+    {
+        return subscribedEventsM;
+    }
+
+    inline
+    void
+    EventHandler::reset_events(
+        unsigned int eventsToReset)
+    {
+        subscribedEventsM &= (~eventsToReset);
+    }
+
+    inline
+    bool
+    EventHandler::is_want_read() const
+    {
+        return (subscribedEventsM & EPOLLIN) != 0;
+    }
+
+    inline
+    bool
+    EventHandler::is_want_write() const
+    {
+        return (subscribedEventsM & EPOLLOUT) != 0;
+    }
+
 }
 
 #endif
